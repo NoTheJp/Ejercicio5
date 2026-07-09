6ce487cb5dbcf954c7352e1b9d4ad886e95a75ac
Jose Alberto Puentes M <josepm0703@gmail.com>
2026-07-08 19:18:58 -0600

    Creación Interfaz EstrategiaEnvio y clase EnvioEstandar (implements EstrategiaEnvio)

---
+++ src/main/java/org/example/EnvioEstandar.java
@@ -0,0 +1,10 @@
+package org.example;
+
+class EnvioEstandar implements EstrategiaEnvio{
+
+    @Override
+    public double calcp(double kg){
+        double costo = kg * 2.00;
+        return costo;
+    }
+}
---
+++ src/main/java/org/example/EstrategiaEnvio.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface EstrategiaEnvio {
+    public double calcp (double kg);
+}
