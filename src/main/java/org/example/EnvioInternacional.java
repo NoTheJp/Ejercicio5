578aa79bb665576ee5273a449cd3e7df9487d3ff
Jose Alberto Puentes M <josepm0703@gmail.com>
2026-07-08 20:41:20 -0600

    agregar lo de cobro unico ps

--- src/main/java/org/example/EnvioInternacional.java
+++ src/main/java/org/example/EnvioInternacional.java
@@ -1,9 +1,22 @@
 package org.example;
 
 class EnvioInternacional implements EstrategiaEnvio {
+    private boolean siono;
+
+    public EnvioInternacional(){
+        this.siono = false;
+    }
+
     @Override
     public double calcp(double kg) {
-        double costo = kg * 6.00 +10.00;
-        return costo;
+        double costo =  kg * 6.00 +10.00;
+
+        if ( this.siono == false){
+            costo = costo + 10.00;
+            this.siono = true;
+            return costo;
+        }else{
+            return costo;
+        }
     }
 }
